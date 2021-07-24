6d95881ff5dae84f364e6d59b16b6b577e0c01a6
Calvin Hu <31859826+chu017@users.noreply.github.com>
2021-07-24 13:47:56 -0700

    update & reload files

--- server/index.js
+++ server/index.js
@@ -1,12 +1,25 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 5000
+const axios = require('axios')
+
+require('dotenv').config()
+const API_KEY = process.env.REACT_APP_API_KEY_TMDB
 
 app.use(express.static('client/dist'));
 
-
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+app.get('/api/movies', (req, res) => {
+  axios.get(`https://api.themoviedb.org/3/movie/550?api_key=${API_KEY}`)
+    .then((response) => {
+      console.log('data from api:', response.data);
+    })
+    .catch((error) => {
+      console.log(error);
+    })
 })
 
 app.listen(port, () => {
